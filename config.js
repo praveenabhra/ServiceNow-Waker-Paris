d285349714f09b775d4631e0a562d75794fe8257
Praveen <praveen.billakanti@abhrainc.com>
2020-11-30 18:51:16 +0530

    Paris Version PDI

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	users : [
+	{
+		"username": "user1@example.com",
+		"password": "user1pass",
+		"notifications": true
+	},
+	{
+		"username": "user2@example.com",
+		"password": "user2pass",
+		"notifications": true
+	}
+	
+	]
+};
